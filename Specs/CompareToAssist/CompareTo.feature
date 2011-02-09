87a2f6a47904412a1f48df098fad31c8e84db990
marcushammarberg <marcusoft.net@gmail.com>
2011-02-10 00:56:02 +0100

    Compare to instance demo

---
+++ Specs/CompareToAssist/CompareTo.feature
@@ -0,0 +1,27 @@
+﻿Feature: Show the compare to feature
+	In order to show the compare to features of SpecFlow Assist
+	As a SpecFlow evanglist
+	I want to show how the different versions of compareTo works
+
+
+#Yes - this is a bug with the date format...
+Scenario: CompareToInstance
+	Given I have the following person
+		| Field		 | Value		|
+		| Name		 | Marcus		|
+		| Style      | Butch		|
+		| Birth date | 1972-10-09	|
+	Then CompareToInstance should match this guy
+		| Field		 | Value					|
+		| Name		 | Marcus					|
+		| Style      | Butch					|
+		| BirthDate  | 10/9/1972 12:00:00 AM	| 
+	And CompareToInstance should match this guy
+		| Field		 | Value					|
+		| Name		 | Marcus					|
+		| BirthDate	 | 10/9/1972 12:00:00 AM	|
+	But CompareToInstance should not match this guy
+		| Field		 | Value					|
+		| Name		 | Anders					|
+		| Style      | very cool				|
+		| BirthDate	 | 10/9/1974 12:00:00 AM	|
---
+++ Specs/CompareToAssist/CompareToSteps.cs
@@ -0,0 +1,39 @@
+﻿using Should.Fluent;
+using Specs.TestEntities;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Specs.CompareToAssist
+{
+    [Binding]
+    public class CompareToSteps
+    {
+        [Given(@"I have the following person")]
+        public void GivenIHaveTheFollowingPerson(Table person)
+        {
+            var p = person.CreateInstance<Person>();
+            ScenarioContext.Current.Set(p);
+        }
+
+        [Then(@"CompareToInstance should match this guy")]
+        public void CompareToInstanceShouldMatch(Table instanceTableToMatch)
+        {
+            var p = ScenarioContext.Current.Get<Person>();
+            instanceTableToMatch.CompareToInstance(p);
+        }
+
+        [Then(@"CompareToInstance should not match this guy")]
+        public void CompareToInstanceShouldNotMatch(Table instanceTableToNotMatch)
+        {
+            var p = ScenarioContext.Current.Get<Person>();
+            try
+            {
+                instanceTableToNotMatch.CompareToInstance(p);
+            }
+            catch (ComparisonException ex)
+            {
+                ex.Message.Should().Not.Be.Empty();
+            }
+        }
+    }
+}
